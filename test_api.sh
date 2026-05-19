0bde345b8ee4bad682381180bfe009477d1ef3cd
Stepan Redka <stepanredka17@gmail.com>
2026-05-19 19:33:46 +0300

    fix: delete the garbage

--- test_api.sh
+++
@@ -1,4 +0,0 @@
-curl -s -X POST http://localhost:5000/api/Chat/Ask \
-     -H "Content-Type: application/json" \
-     -d '{"question":"What is RAG?","topK":3,"isHighFidelity":false}' > /home/stepan/Documents/Uni/4course/Diploma/RagSystem/api_out.json
-cat /home/stepan/Documents/Uni/4course/Diploma/RagSystem/api_out.json
--- test_server.sh
+++
@@ -1,2 +0,0 @@
-cd /home/stepan/Documents/Uni/4course/Diploma/RagSystem/Diploma.Web
-dotnet build
